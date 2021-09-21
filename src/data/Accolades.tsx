af28a4facc787c230c01695065641853611ddec2
maxmagyar <mrmagyar@uwm.edu>
2021-09-21 11:44:18 -0500

    Update Accolades.tsx

--- src/data/Accolades.tsx
+++ src/data/Accolades.tsx
@@ -5,13 +5,13 @@ export const PlayerAccolades: Array<PlayerAccolade> = [
     id: "qb",
     title: "Top Quarter Back Performance",
     description: "",
-    imageUrl: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3139477.png",
+    imageUrl: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3917315.png&w=350&h=254",
     payout: 10,
     player: {
-      firstName: "Patrick",
-      lastName: "Mahomes II",
-      points: 38.65,
-      team: "KC"
+      firstName: "Kyler",
+      lastName: "Murray",
+      points: 42.1,
+      team: "ARI"
     },
     team: {
       name: "Mooney Knight",
@@ -20,27 +20,27 @@ export const PlayerAccolades: Array<PlayerAccolade> = [
         lastName: "Backes"
       }
     },
-    week: 1
+    week: 2
   }, {
     id: "rb",
     title: "Top Running Back Performance",
     description: "",
-    imageUrl: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3117251.png",
+    imageUrl: "https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/3043078.png&w=350&h=254",
     payout: 10,
     player: {
-      firstName: "Christian",
-      lastName: "McCaffrey",
-      points: 28.7,
-      team: "CAR"
+      firstName: "Derrick",
+      lastName: "Henry",
+      points: 50.7,
+      team: "TEN"
     },
     team: {
-      name: "James the Conqueror",
+      name: "J.A.R.V.I.S. Landry",
       user: {
-        firstName: "Brett",
-        lastName: "Farquhar"
+        firstName: "Max",
+        lastName: "Magyar"
       }
     },
-    week: 1
+    week: 2
   }, {
     id: "wrte",
     title: "Top Receiver (WR or TE) Performance",
@@ -69,22 +69,22 @@ export const TeamAccolades: Array<Accolade> = [
     id: "oddball",
     title: "Oddball",
     description: "",
-    imageUrl: "https://scontent-ort2-1.xx.fbcdn.net/v/t1.6435-9/145092012_3869476673147156_2006371949438883827_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=xfI0WfejNpEAX_PbrVC&_nc_oc=AQkBzfEilIlX6eFeZR39lxrsqISCwHIE0avfknZYJf_5NFEOW4xzni23sgdAuUTwoMsmmSXr35dMpAgdFrJch-tI&_nc_ht=scontent-ort2-1.xx&oh=e90a20a7b9de3ec3646fad91d125fbfc&oe=6168F6F5",
+    imageUrl: "https://scontent-msp1-1.xx.fbcdn.net/v/t1.18169-9/c56.0.362.362a/263741_10150232853006749_7470933_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=da31f3&_nc_ohc=Ct1blpPv1dwAX8ZiEf3&_nc_ht=scontent-msp1-1.xx&oh=5109f6f8214fe1619e45d8583bb0a904&oe=616DB3D4",
     payout: 50,
     team: {
-      name: "James the Conqueror",
+      name: "A.J. Dillmonger",
       user: {
-        firstName: "Brett",
-        lastName: "Farquhar"
+        firstName: "Danny",
+        lastName: "Rajchel"
       }
     },
-    week: 1
+    week: 2
   },
   {
     id: "team",
     title: "Top Scoring Team",
-    description: "",
-    imageUrl: "https://scontent-ort2-1.xx.fbcdn.net/v/t1.6435-9/145092012_3869476673147156_2006371949438883827_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=xfI0WfejNpEAX_PbrVC&_nc_oc=AQkBzfEilIlX6eFeZR39lxrsqISCwHIE0avfknZYJf_5NFEOW4xzni23sgdAuUTwoMsmmSXr35dMpAgdFrJch-tI&_nc_ht=scontent-ort2-1.xx&oh=e90a20a7b9de3ec3646fad91d125fbfc&oe=6168F6F5",
+    description: "Matchup: AJ Dillmonger",
+    imageUrl: "https://scontent-msp1-1.xx.fbcdn.net/v/t1.6435-9/c0.79.720.720a/s552x414/59921617_10217394593790551_4218093587629342720_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=da31f3&_nc_ohc=DWU5MkW_k_8AX_90lRt&_nc_ht=scontent-msp1-1.xx&oh=85fb4766a48ba2ff8452b2b54132ff47&oe=61709100",
     payout: 25,
     team: {
       name: "James the Conqueror",
